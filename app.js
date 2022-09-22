62b24b76fec217cae2f4e6e9c7c43ba692a8db58
Pixel <nikodems1308@gmail.com>
2022-09-22 23:04:20 +0200

    deleting layers work

--- app.js
+++ app.js
@@ -16,8 +16,8 @@
     let frontIconIndex;
     let backIconIndex;
     let createBin;
-    let divRetriangle;
-    let x;
+    let divRectangle;
+    let x,y;
     // Get modal element
     let modal = document.getElementById('modal');
     // Get open button
@@ -33,7 +33,8 @@
     let imgId;
     
     function openModal(e) {
-        x = 0;
+        // x = 0;
+        // y = 0;
         imgId = e.target.id;
         console.log(imgId);
         modal.style.display = "flex";
@@ -132,16 +133,6 @@
         e.preventDefault();
     }
 
-    // Listening frontSideBtn
-
-    frontSide.addEventListener('click', btnValue)
-
-    function btnValue(e) {
-        x = 0;
-        frontSideBtn = true;
-        e.preventDefault();
-    }
-
     //Store images
 
     function storeImageInSessionStorage(imageSrc, fileName, elementId) {
@@ -175,16 +166,18 @@
     function deleteFromSessionStorage(imageToDelete) {
         let frontSessionStorage;
         let backSessionStorage;
-        frontSessionStorage = JSON.parse(sessionStorage.getItem('front-Tshirt'));
-        frontSessionStorage.forEach(function (item, index) {
+        if(frontSideBtn == true) {
+        frontSessionStorage = JSON.parse(sessionStorage.getItem(imgId));
+        frontSessionStorage.forEach((item, index) => {
             if (imageToDelete == index) {
                 frontSessionStorage.splice(index, 1);
             }
         });
-            sessionStorage.setItem('front-Tshirt', JSON.stringify(frontSessionStorage));       
-        if (sessionStorage.getItem("back-Tshirt") == null) {
+            sessionStorage.setItem(imgId, JSON.stringify(frontSessionStorage));   
+        } else {    
+            if (sessionStorage.getItem("back-Tshirt") == null) {
             backSessionStorage = [];
-        } else {
+            } else {
             backSessionStorage = JSON.parse(sessionStorage.getItem('back-Tshirt'));
         }
         backSessionStorage.forEach(function (item, index) {
@@ -193,7 +186,7 @@
             }
         });
         sessionStorage.setItem('back-Tshirt', JSON.stringify(backSessionStorage));
-        
+    }
     }
             
     // Getting images from SessionStorage
@@ -220,13 +213,16 @@
               console.log(frontSessionStorage);
             let names = [];
             frontSessionStorage.forEach((image, i) => {
-                showFrontImagesAndDivRetriangle(image, i);
+                showFrontImagesAndDivRectangle(image, i);
                 });
         }}
 
         // Getting backSide images from SessionStorage
 
         function getBackImagesFromSessionStorage(elementId){
+
+        // Creating chosen item image for backSide
+
         let backSideImage = document.createElement("img");
         let backImgId = imgId.split("-");
         console.log(backImgId);
@@ -235,65 +231,67 @@
         backSideImage.className = "background back-canvas-img left-side";
         document.getElementById("left-side").appendChild(backSideImage);
 
+        // Creating background for item image
+
         let backSideBackground = document.createElement("img");
         backSideBackground.setAttribute("src", "items/back-" + backImgId[1] + "Background.png");
         backSideBackground.id = "back-background";
         backSideBackground.className = "background back-canvas-img left-side";
         document.getElementById("left-side").appendChild(backSideBackground);
-          let backSessionStorage;
-          let backId = elementId.split("-");
-          console.log(backId);
-          if(sessionStorage.getItem("back-"+backId[1]) == null){
+        let backSessionStorage;
+        let backId = elementId.split("-");
+        console.log(backId);
+
+        // Checking session storage 
+
+        if(sessionStorage.getItem("back-"+backId[1]) == null){
             backSessionStorage = [];
-          } else {
+        } else {
             backSessionStorage = JSON.parse(sessionStorage.getItem("back-"+backId[1]));
-            x = 0;
+            y = 0;
             backSessionStorage.forEach((image, i) => {
-            showBackImagesAndDivRetriangle(image, i, x);
+            showBackImagesAndDivRectangle(image, i);
           });}
         }
 
-                function showFrontImagesAndDivRetriangle(image){
-                
-                
-                divRetriangle = document.createElement('div');        
-                divRetriangle.className = "front-index";
-                frontIconIndex = document.getElementsByClassName("front-index").length;
-                console.log(frontIconIndex);
-                divRetriangle.id = "front-index" + frontIconIndex;       
-                holder.appendChild(divRetriangle);
-                    names = image.split("-");  
-                    let imgObj = new Image();
-                    imgObj.src = names[0];
-                    
-                     console.log(imgObj);
-                    
-                    imgObj.onload = function () {
-                        
-                        console.log(x);
-                        let newImg = new fabric.Image(imgObj);
-                        
-                        newImg.scaleToHeight(150);
-                        newImg.scaleToWidth(150);
-                        newImg.visible = true;                        
-                        frontCanvas.centerObject(newImg);
-                        frontCanvas.add(newImg);
-                        
-                        frontCanvas.item(x).moveTo(0-x);
-                        console.log(frontCanvas.item(x));
-                        x++;
-                        console.log(x);
-                       
-                        frontCanvas.renderAll();
-                                          
-                    }
-                     
-                        
-                     console.log(names[0]);
-                    
+        function showFrontImagesAndDivRectangle(image){
+
+            // Creating container to display layer
+
+            divRectangle = document.createElement('div');        
+            divRectangle.className = "front-index";
+            frontIconIndex = document.getElementsByClassName("front-index").length;
+            console.log(frontIconIndex);
+            divRectangle.id = "front-index" + frontIconIndex;       
+            holder.appendChild(divRectangle);
+            names = image.split("-");
+
+            // Creating image object for frontCanvas
+
+            let imgObj = new Image();
+            imgObj.src = names[0];
+            console.log(imgObj);
+            imgObj.onload = function () {
+                console.log(x);
+                let newImg = new fabric.Image(imgObj);
+                newImg.scaleToHeight(150);
+                newImg.scaleToWidth(150);
+                newImg.visible = true;                        
+                frontCanvas.centerObject(newImg);
+                frontCanvas.add(newImg);
+                frontCanvas.item(x).moveTo(0-x);
+                console.log(frontCanvas.item(x));
+                x++;
+                console.log(x);
+                frontCanvas.renderAll();
+            }
+            console.log(names[0]);
+
+            // Creating Image holder
+
             let miniImageHolder = document.createElement('div');
             miniImageHolder.className = "image-holder"
-            divRetriangle.appendChild(miniImageHolder);
+            divRectangle.appendChild(miniImageHolder);
 
             // Creating image element
 
@@ -316,7 +314,7 @@
 
             // Selecting div to put element into div
 
-            divRetriangle.appendChild(text);
+            divRectangle.appendChild(text);
 
             // Creating image element
 
@@ -327,7 +325,7 @@
             let rightSideHolder = document.createElement("div");
             rightSideHolder.style.width = "85px";
             rightSideHolder.style.float = "left";
-            divRetriangle.appendChild(rightSideHolder);
+            divRectangle.appendChild(rightSideHolder);
 
             // Styling showHideIcon 
 
@@ -360,45 +358,53 @@
             
             // Listening bin image  
 
-             createBin.addEventListener("click", deleteElement);
-            }
+            createBin.addEventListener("click", deleteElement);
+        }
 
-    function showBackImagesAndDivRetriangle(image){
+    function showBackImagesAndDivRectangle(image){
+
+        // Splitting name for get side of canvas
+
+        names = image.split("-"); 
         
-        names = image.split("-");  
+        // Creating image object for canvas
+
         let imgObj = new Image();
         imgObj.src = names[0];
         imgObj.onload = function () {
-            
-            console.log(x);
+            console.log(y);
             let newImg = new fabric.Image(imgObj);
             newImg.scaleToHeight(150);
             newImg.scaleToWidth(150);
             newImg.visible = true;
             backCanvas.centerObject(newImg);
             backCanvas.add(newImg);
-            console.log(backCanvas.item(x));
-            backCanvas.item(x).moveTo(0-x);
-            
-            x++;
+            console.log(backCanvas.item(y));
+            backCanvas.item(y).moveTo(0-y);
+            y++;
             backCanvas.renderAll();            
         }
         console.log(names[0]);
-        divRetriangle = document.createElement('div');
-        divRetriangle.className = "back-index";
+
+        //Creating layer in files holder
+
+        divRectangle = document.createElement('div');
+        divRectangle.className = "back-index";
         backIconIndex = document.getElementsByClassName("back-index").length;
-        divRetriangle.id = "back-index" + backIconIndex;
-        holder.appendChild(divRetriangle);
+        divRectangle.id = "back-index" + backIconIndex;
+        holder.appendChild(divRectangle);
+
+        // Creating image holder
             
         let miniImageHolder = document.createElement('div');
         miniImageHolder.className = "image-holder";
-        divRetriangle.appendChild(miniImageHolder);
+        divRectangle.appendChild(miniImageHolder);
 
         // Creating image element
 
         let miniImage = document.createElement('img');
 
-        // Assigment loaded image to variable
+        // loading image file to variable
 
         console.log(names[0]);
         miniImage.setAttribute("src", names[0]);
@@ -417,7 +423,7 @@
 
         // Selecting div to put element into div
 
-        divRetriangle.appendChild(text);
+        divRectangle.appendChild(text);
 
         // Creating image element
 
@@ -428,7 +434,7 @@
         let rightSideHolder = document.createElement("div");
         rightSideHolder.style.width = "85px";
         rightSideHolder.style.float = "left";
-        divRetriangle.appendChild(rightSideHolder);
+        divRectangle.appendChild(rightSideHolder);
 
         // Styling showHideIcon 
 
@@ -449,6 +455,7 @@
         // Styling bin image
 
         createBin.setAttribute("src", "items/bin.png");
+
         // Adding id and className to bin image
 
         createBin.id = "back-bin" + backIconIndex;
@@ -462,18 +469,6 @@
 
          createBin.addEventListener("click", deleteElement);
     }
-
-    // Listening backSideBtn
-
-    backSide.addEventListener('click', backBtnValue);
-
-    function backBtnValue(e) {
-        x = 0;
-        console.log(x);
-        frontSideBtn = false;
-        e.preventDefault();
-    }
-
         
     // Uploading image file and creating object
     
@@ -486,25 +481,24 @@
             let imgObj = new Image();
             imgObj.src = e.target.result;
             console.log (imgObj.src);
-             console.log(realBtn.files[arrayLength].name);
+            console.log(realBtn.files[arrayLength].name);
             storeImageInSessionStorage(imgObj.src, realBtn.files[arrayLength].name, imgId);
-            divRetriangle = document.createElement('div');
+            divRectangle = document.createElement('div');
         if (frontSideBtn == true) {
-            divRetriangle.className = "front-index";
-            frontIconIndex = document.getElementsByClassName("front-index").length;
-            divRetriangle.id = "front-index" + frontIconIndex;
-
+          divRectangle.className = "front-index";
+          frontIconIndex = document.getElementsByClassName("front-index").length;
+          divRectangle.id = "front-index" + frontIconIndex;
         } else {
-            divRetriangle.className = "back-index";
-            backIconIndex = document.getElementsByClassName("back-index").length;
-            divRetriangle.id = "back-index" + backIconIndex;
+          divRectangle.className = "back-index";
+          backIconIndex = document.getElementsByClassName("back-index").length;
+          divRectangle.id = "back-index" + backIconIndex;
         }
         
-        holder.appendChild(divRetriangle);
+        holder.appendChild(divRectangle);
 
         let miniImageHolder = document.createElement('div');
         miniImageHolder.className = "image-holder";
-        divRetriangle.appendChild(miniImageHolder);
+        divRectangle.appendChild(miniImageHolder);
 
         // Creating image element
 
@@ -528,7 +522,7 @@
 
         // Selecting div to put element into div
 
-        divRetriangle.appendChild(text);
+        divRectangle.appendChild(text);
 
         // Creating image element
 
@@ -539,7 +533,7 @@
         let rightSideHolder = document.createElement("div");
         rightSideHolder.style.width = "85px";
         rightSideHolder.style.float = "left";
-        divRetriangle.appendChild(rightSideHolder);
+        divRectangle.appendChild(rightSideHolder);
 
         // Styling showHideIcon 
 
@@ -549,10 +543,10 @@
             showIcon.className = "frontIcon";
             showHideFront[frontIconIndex] = true;
         } else {
-            showIcon.id = "backIcon" + backIconIndex;
-            showIcon.className = "backIcon";
-            showHideBack[backIconIndex] = true;
-            }
+          showIcon.id = "backIcon" + backIconIndex;
+          showIcon.className = "backIcon";
+          showHideBack[backIconIndex] = true;
+        }
 
         // Listening event on icon
 
@@ -592,18 +586,19 @@
                 newImg.scaleToWidth(150);
                 newImg.visible = true;
                 if (frontSideBtn == true) {
+                    x = document.querySelectorAll(".front-index").length - 1;
+                    console.log(x);
                     frontCanvas.centerObject(newImg);
                     frontCanvas.add(newImg);
                     frontCanvas.item(x).moveTo(0-x);
                     console.log(frontCanvas.item(x));
-                    x++;
                     frontCanvas.renderAll();
                 } else {
+                    y = document.querySelectorAll(".back-index").length - 1;
                     backCanvas.centerObject(newImg);
                     backCanvas.add(newImg);
-                    backCanvas.item(x).moveTo(0-x);
-                    console.log(backCanvas.item(x));
-                    x++;
+                    backCanvas.item(y).moveTo(0-y);
+                    console.log(backCanvas.item(y));
                     backCanvas.renderAll();
                 }
             }
@@ -617,7 +612,7 @@
         if (frontSideBtn == true) {
             let getElement = e.target.id;
             let idText = getElement.match(/\d/g).join('');
-            frontCanvas.remove(frontCanvas.item(idText));
+            frontCanvas.remove(frontCanvas.item(document.querySelectorAll(".front-index").length - idText - 1));
             frontCanvas.renderAll();
             document.getElementById("front-index" + idText).remove();
             deleteFromSessionStorage(idText);
@@ -630,7 +625,7 @@
         } else {
             let getElement = e.target.id;
             let idText = getElement.match(/\d/g).join('');
-            backCanvas.remove(backCanvas.item(idText));
+            backCanvas.remove(backCanvas.item(document.querySelectorAll(".back-index").length - idText - 1));
             backCanvas.renderAll();
             document.getElementById("back-index" + idText).remove();
             let binElements = document.querySelectorAll(".back-index");
@@ -682,7 +677,8 @@
     function changeToFrontSide(e) {
 
         // Changing backSideElements to frontSideElements
-
+        x = 0;
+        frontSideBtn = true;
         document.getElementsByClassName("canvas-container")[0].style.zIndex = "101";
         document.getElementsByClassName("canvas-container")[1].style.zIndex = "100";
         document.getElementById('front').style.display = 'inline';
@@ -740,7 +736,8 @@
     function changeToBackSide(e) {
 
         // Changing frontSideElements to backSideElements
-
+        y = 0;
+        frontSideBtn = false;
         document.getElementsByClassName("canvas-container")[0].style.zIndex = "100";
         document.getElementsByClassName("canvas-container")[1].style.zIndex = "101";
         document.getElementById('front').style.display = 'none';
