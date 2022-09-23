b8af890b7377aeb39244ac01f7e396aef15a3833
Pixel <nikodems1308@gmail.com>
2022-09-23 14:26:36 +0200

    color picker function

---
+++ color-picker.js
@@ -0,0 +1,28 @@
+// Get color from input
+
+let pickerColor = document.getElementById('pallet');
+let inputColor = document.getElementById('hexa-color');
+
+//Getting color value from input
+
+pickerColor.addEventListener('input', colorValue);
+
+function colorValue(e) {
+    const backColor = document.getElementById('back-background');
+    let frontColor = document.getElementById('front-background');
+    backColor.style.backgroundColor = pickerColor.value;
+    frontColor.style.backgroundColor = pickerColor.value;
+    inputColor.value = pickerColor.value;
+    e.preventDefault();
+}
+
+inputColor.addEventListener('input', inputValue);
+
+function inputValue(e) {
+    const backColor = document.getElementById('back-background');
+    const frontColor = document.getElementById('front-background');
+    frontColor.style.backgroundColor = inputColor.value;
+    backColor.style.backgroundColor = inputColor.value;
+    pickerColor.value = inputColor.value;
+    e.preventDefault();
+}
