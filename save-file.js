370c143fabdd6534f2aed4b666e854692f56d8ab
Pixel <nikodems1308@gmail.com>
2022-09-23 14:38:39 +0200

    delete

--- save-file.js
+++
@@ -1,7 +0,0 @@
-const saveBtn = document.getElementById("saveBtn");
-    saveBtn.addEventListener('click', function () {
-        domtoimage.toBlob(document.getElementById('left-side'))
-            .then(function (blob) {
-                window.saveAs(blob, 'my-node.png');
-            });
-    });
